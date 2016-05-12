9dd667ea0bc3facdce0e1cb4112dfe819ea4413a
Maxim <sfcl@mail.ru>
2016-05-12 23:39:01 +0500

    Попытки работать с бин файлами

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <conio.h>
 #include <windows.h>
+#include <fstream>
 
 using namespace std;
 
@@ -49,16 +50,39 @@ class DataTable {
 };
 
 DataTable::DataTable() {
+     /* Производим иницализацию данных начальными значениями.
+        Считываем данные из файла data.db 
+     */
      this->table_length = 0;
      this->id = 1;
      this->first_index = 1;
      this->change = false;
+     main_table row;
+     ifstream f;
+     f.open("data.db", ios::binary);
+     if (f) {
+        while (f.good()) {
+            if( !f.read((char *) &row, sizeof(row)) )
+                break; 
+            f.read((char *) &row, sizeof(row));
+            cout << row.title <<  row.place << row.year << endl; getch();
+            /*this->add_line(row.title, row.place, row.year); */
+        }
+        
+     } else { 
+         /* если файл базы данных отсутствует, то создаём пустой */
+         cout << "Не найден файл данных!" << endl; getch();
+         ofstream f("data.db");
+         f.close();
+         exit(1);
+     }
+     f.close(); // закрываем файл
 }
 
 void DataTable::show() {
     gotoxy(0, 0);
     cout << "+----+-----------------------------+----------------------------+-------------+" << endl;
-    cout << "| ID |    Ќ §ў ­ЁҐ                 |  ЊҐбв®Ї®«®¦Ґ­ЁҐ            | ѓ®¤ б®§¤ ­Ёп|" << endl;              
+    cout << "| ID |    Название                 |  Место положения           |Год создания |" << endl;              
     cout << "+----+-----------------------------+----------------------------+-------------+" << endl;                  
     cout << "|    |                             |                            |             |" << endl;
     cout << "|    |                             |                            |             |" << endl;
@@ -78,24 +102,31 @@ void DataTable::show() {
     cout << "+----+-----------------------------+----------------------------+-------------+" << endl;
     int len = this->table_length;
     int id = this->first_index;
-    for (int i=1; i <(TABLE_ROWS+1); i++, id++) {
-        gotoxy(2, 3+((i-1)*2));
-        cout << this->data[id].id << endl;
-        
-        gotoxy(6, 3+((i-1)*2));
-        cout << this->data[id].title << endl;
-        
-        gotoxy(36, 3+((i-1)*2));
-        cout << this->data[id].place << endl;
-        
-        gotoxy(65, 3+((i-1)*2));
-        cout << this->data[id].year << endl;
-        
-        gotoxy(0, 4+((i-1)*2));
-        cout << "+----+-----------------------------+----------------------------+-------------+" << endl;    
+    if (len) {
+        if (len <= TABLE_ROWS) {
+            len = len + 1;
+        } else {
+            len = TABLE_ROWS + 1;   
         }
-        gotoxy(0, 24);
-        cout << "‚ўҐ¤ЁвҐ Є®¬ ­¤г -> ";
+        for (int i=1;i < len; i++, id++) {
+            gotoxy(2, 3+((i-1)*2));
+            cout << this->data[id].id << endl;
+            
+            gotoxy(6, 3+((i-1)*2));
+            cout << this->data[id].title << endl;
+            
+            gotoxy(36, 3+((i-1)*2));
+            cout << this->data[id].place << endl;
+            
+            gotoxy(65, 3+((i-1)*2));
+            cout << this->data[id].year << endl;
+            
+            gotoxy(0, 4+((i-1)*2));
+            cout << "+----+-----------------------------+----------------------------+-------------+" << endl;    
+        }
+    }
+    gotoxy(0, 24);
+    cout << "Введите команду -> ";
 };   
 
 void DataTable::data_down() {
@@ -131,13 +162,9 @@ void DataTable::clear_table() {
 }
 
 void DataTable::add_line(string title, string place, int year) {
-    /* Њ ЄбЁ¬ «м­®Ґ Є®«ЁзҐбвў® ®в®Ўа ¦ Ґ¬ле бва®Є 8 
-    Ґб«Ё Ў®«миҐ в® Їа®Ё§ў®¤Ё¬ ЇҐаҐЈагЇЇЁа®ўЄг в Ў«Ёжл
-     */
     this->table_length++;
     this->change = true;
     int len = this->table_length;
-    /* Є®бўҐҐ­­®Ґ ўлзЁб«Ґ­ЁҐ Є®®а¤Ё­ вл б«Ґ¤гойҐ© бва®ЄЁ */
     this->data[len].id    = this->id;
     this->data[len].title = title;
     this->data[len].place = place;
@@ -154,9 +181,11 @@ class Dialog {
         DataTable * tab;
         
     public:
-        Dialog(DataTable * tab);
         void show();
         void save();
+        void show_help();
+        Dialog(DataTable * tab);
+        Dialog();
 };
 
 void Dialog::show() {
@@ -165,12 +194,12 @@ void Dialog::show() {
     string title1 = "";
     string place1 = "";
     int year1;
-    cout << "Add new cultural item" << endl << endl;
+    cout << "Добавление памятника архитектуры Санкт-Петербурга" << endl << endl;
     cin.clear();
-    cout << "Title = "; getline(cin, title1);
-    cout << "Place = "; getline(cin, place1);
-    cout << "Year =  "; cin >> year1;
-    cout << "Save? [y/n] "; ch = getch();
+    cout << "Название      "; getline(cin, title1);
+    cout << "мето          "; getline(cin, place1);
+    cout << "Год создания  "; cin >> year1;
+    cout << "Сохранить? [y/n] "; ch = getch();
     if (ch == 'y') {
         /* Save data in table */ 
         this->tab->add_line(title1, place1, year1);
@@ -180,6 +209,19 @@ void Dialog::show() {
         this->tab->show();       
     } 
 }
+
+void Dialog::show_help() {
+    clear_all();
+    char ch;
+    ifstream input("help.txt");
+    string line;
+    while( getline( input, line ) ) {
+        cout << line << endl;
+    }
+    ch = getch();
+            
+}
+
 
 void Dialog::save() {
      clear_all();
@@ -199,12 +241,16 @@ Dialog::Dialog(DataTable * tab) {
     this->tab = tab; 
 }
 
+Dialog::Dialog() {
+    /* empty contructor */ 
+}
 
 int main(int argc, char *argv[])
 {
+    setlocale(LC_ALL, "Russian");
     char c;
     DataTable t1;
-    t1.add_line("dfdf", "23213", 1998);
+/*    t1.add_line("dfdf", "23213", 1998);
     t1.add_line("dsdsd", "sdsd13", 1999);
     t1.add_line("dsdsd", "sdsd13", 1999);
     t1.add_line("dsdsd", "sdsd13", 1999);
@@ -214,14 +260,14 @@ int main(int argc, char *argv[])
     t1.add_line("dsdsd", "sdsd13", 1999);
     t1.add_line("aaaaaaa", "aaaaaaaa", 2000);
     t1.add_line("bbbbbbb", "vvvvvvvv", 2001);
-    t1.add_line("ccccccc", "wwwwwwww", 2001);
+    t1.add_line("ccccccc", "wwwwwwww", 2001); */
     t1.show();
     int up = 0;
     unsigned int ch;
     while(1) {
       if(kbhit()) {
            ch = getch();
-           if ((ch == 'e') || (ch == 'E')) {
+           if (ch == 27) {  // выходим из программы клавишей Esc
               if (t1.change) {
                  Dialog dialog(& t1);
                  dialog.save();
@@ -240,22 +286,28 @@ int main(int argc, char *argv[])
                   cout << --up;
               }
            }
-           if (ch ==  110) { // яcreate new object by n key
+           if (ch ==  110) {
                Dialog dialog(& t1);
                dialog.show();
            }
            
-           if (ch == 113) { // scroll table up
+           if ((ch == 'q') || (ch == 'Q')) { // пролистывание содержимого вверх
                t1.data_up();   
            }
            
-           if (ch == 97) { // scroll table donwn
+           if ((ch == 'a') || (ch == 'A')) { // пролистывание таблицы вниз
               t1.data_down();              
+           }
+           
+           if ((ch == 'h') || (ch == 'H')) { // справка по использованию программы
+               Dialog help;
+               help = Dialog();
+               help.show_help();
+               t1.show();       
            }
            gotoxy(30, 24);  cout << (int) ch;
            while(kbhit()) getch();  //clear the buffer again although it is not necessary, remove this to make the game smoother
        }
-       //add a wait here if you want
     }
     return 0;
 }
